7abf179235b2455a97190fe88c179c39835e8256
RAC IT <racit@192.168.1.33>
2025-11-14 00:48:54 +0530

    day6 assignments

--- day6_assignments/SmartLibraryManagementSystem/library/items/ItemType.scala
+++ day6_assignments/SmartLibraryManagementSystem/library/items/ItemType.scala
@@ -3,7 +3,6 @@ package library.items
 sealed trait ItemType {
   def title: String
 }
-
 case class Book(title: String) extends ItemType
 case class Magazine(title: String) extends ItemType
 case class DVD(title: String) extends ItemType
