a9ad0f87ccd66422b89783adabc7e0b9776cb0b5
naresh-scala-dev <nagulanaresh24@gmail.com>
2025-11-17 10:25:14 +0530

    Delete Consumer directory

--- Consumer/src/main/scala/Person.scala
+++
@@ -1,9 +0,0 @@
-import spray.json._
-
-case class Person(sno: Int, name: String, city: String)
-
-object JsonFormats {
-  import DefaultJsonProtocol._
-
-  implicit val personFormat: RootJsonFormat[Person] = jsonFormat3(Person)
-}
